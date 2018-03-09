21be3a073b253a9446922c2cd7986f7517a82b21
pjullrich <peter.j.ullrich@gmail.com>
2018-03-09 14:52:39 +0100

    Added initial onboarding functionality for Student Added dependencies to Student Maven config

--- studybits-student/src/main/java/nl/quintor/studybits/student/Student.java
+++ studybits-student/src/main/java/nl/quintor/studybits/student/Student.java
@@ -6,7 +6,6 @@ import nl.quintor.studybits.indy.wrapper.IndyWallet;
 import nl.quintor.studybits.indy.wrapper.Prover;
 import nl.quintor.studybits.indy.wrapper.dto.AnoncryptedMessage;
 import nl.quintor.studybits.indy.wrapper.dto.ConnectionRequest;
-import org.hyperledger.indy.sdk.IndyException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,9 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
 import java.util.HashMap;
-import java.util.concurrent.ExecutionException;
 
 @AllArgsConstructor
 @RestController
@@ -27,7 +24,7 @@ public class Student {
     private IndyWallet indyWallet;
 
     @RequestMapping(value = "/onboard", method = RequestMethod.POST)
-    public ResponseEntity<String> onboard(@RequestParam(value = "endpoint") String uniEndpoint) throws IOException, InterruptedException, ExecutionException, IndyException {
+    public ResponseEntity<String> onboard(@RequestParam(value = "endpoint") String uniEndpoint) throws Exception {
         ResponseEntity<ConnectionRequest> connectionRequest = new RestTemplate().getForEntity(uniEndpoint, ConnectionRequest.class, new HashMap<>().put("name", email));
         AnoncryptedMessage connectionResponse = prover.acceptConnectionRequest(connectionRequest.getBody()).get();
 
