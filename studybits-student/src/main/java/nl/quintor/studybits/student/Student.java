987982112137891c5d554e8a82e6341d0e88f660
pjullrich <peter.j.ullrich@gmail.com>
2018-03-09 13:59:17 +0100

    Added initial onboarding functionality for Student Added dependencies to Student Maven config

--- studybits-student/src/main/java/nl/quintor/studybits/student/Student.java
+++ studybits-student/src/main/java/nl/quintor/studybits/student/Student.java
@@ -1,4 +1,38 @@
 package nl.quintor.studybits.student;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import nl.quintor.studybits.indy.wrapper.IndyWallet;
+import nl.quintor.studybits.indy.wrapper.Prover;
+import nl.quintor.studybits.indy.wrapper.dto.AnoncryptedMessage;
+import nl.quintor.studybits.indy.wrapper.dto.ConnectionRequest;
+import org.hyperledger.indy.sdk.IndyException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.concurrent.ExecutionException;
+
+@AllArgsConstructor
+@RestController
 public class Student {
+    @Getter
+    private String email;
+    private Prover prover;
+    private IndyWallet indyWallet;
+
+    @RequestMapping(value = "/onboard", method = RequestMethod.POST)
+    public ResponseEntity<String> onboard(@RequestParam(value = "endpoint") String uniEndpoint) throws IOException, InterruptedException, ExecutionException, IndyException {
+        ResponseEntity<ConnectionRequest> connectionRequest = new RestTemplate().getForEntity(uniEndpoint, ConnectionRequest.class, new HashMap<>().put("name", email));
+        AnoncryptedMessage connectionResponse = prover.acceptConnectionRequest(connectionRequest.getBody()).get();
+
+        return new RestTemplate().getForEntity(uniEndpoint, String.class, new HashMap<>().put("response", connectionResponse));
+    }
+
+
 }
