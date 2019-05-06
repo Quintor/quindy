01512e7d85e422ead77e36dd0243386fcaafa693
Pim Otte <potte@quintor.nl>
2019-05-06 11:53:10 +0200

    Switch over to circleci

--- .travis.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-if [ ! -f $HOME/.m2/settings.xml ]; then
-    echo '<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0 http://maven.apache.org/xsd/settings-1.0.0.xsd"></settings>' > $HOME/.m2/settings.xml
-fi
-export TEST_POOL_IP=127.0.0.1
-docker-compose up --build --force-recreate --exit-code-from wrapper pool wrapper
